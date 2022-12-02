b4b70ee5fa30070bfe8133cb80d8755ec5839195
Aman Sharma <98725523+amansharma049@users.noreply.github.com>
2022-12-02 16:14:57 +0530

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,15 @@
+ // Get the modal
+ var form = document.getElementById('sheetdb-form');
+ form.addEventListener("submit", e =>{
+     e.preventDefault();
+     fetch(form.action, {
+         method: "POST",
+         body: new FormData(document.getElementById("sheetdb-form")),
+             
+     }).then(
+         response => response.json()
+     ).then((html) => {
+         window.open("submit.html","_blank");
+     });
+ });
+     
